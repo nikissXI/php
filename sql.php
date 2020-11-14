0740c04a8dfa5c0d7726f2618616f81e6bd04a99
nikissXI <1299577815@qq.com>
2020-11-14 19:39:33 +0800

    1

--- sql.php
+++ sql.php
@@ -1,8 +1,11 @@
 <html>
 
 <?
-$dsn = "Driver={SQL Server};Server=192.168.137.129;Database=sqlks"; //sql服务器IP,数据库
-$conn = odbc_connect($dsn, 'nikiss', 'nikisss'); //sql账号,密码
+$host = "192.168.137.129";
+$username = "nikiss";
+$password = "nikisss";
+$dsn = "Driver={SQL Server};Server='$host';Database=sqlks"; //sql服务器IP,数据库
+$conn = odbc_connect($dsn, $username, $password); //sql账号,密码
 ?>
 
 </html>
